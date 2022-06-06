7df3a3abb725be3fca7672fd99b1e3648d89ffad
SamaraBilby <samarabilby>
2022-06-05 22:04:45 -0400

    primeiro commit

---
+++ js/script.js
@@ -0,0 +1,58 @@
+const menu = [
+    {
+    id: 1,
+    title: 'Sanduiche Frango',
+    categoria: 'Sanduiche',
+    preco: 11.23,
+    imagem: '../assets/image/sanduiche/sanduiche-frango.jpg',
+    descricao: 'Iscas de peito de frango, alface, tomate, queijo e maionese americana',
+}
+{
+    id: 2,
+    title: 'Sanduiche Carne',
+    categoria: 'Sanduiche',
+    preco: 15.59,
+    imagem: '../assets/image/sanduiche/sanduiche-chapa.jpg',
+    descricao: '100 g do hamburger 100% carne Angus, 2 fatias de muçarela derretida, alface, tomate fresquinho, picles, cebola e maionese à sua escolha.',
+}
+{
+    id: 3,
+    title: 'Sanduiche carne em dobro',
+    categoria: 'Sanduiche',
+    preco: 25.00,
+    imagem: '../assets/image/sanduiche/sanduiche-duplo-batata.jpg',
+    descricao: '2 hambúrguer Angus 100% carne, picles, alface-americana,2 fatias bacon, cebola picada, 2 fatias de muçarela, maionese e porção de batata-frita',
+}
+{
+    id: 1,
+    title: 'Sanduiche Frango',
+    categoria: 'Sanduiche',
+    preco: 11.23,
+    imagem: '../assets/image/sanduiche/sanduiche-frango.jpg',
+    descricao: 'Iscas de peito de frango, alface, tomate, queijo e maionese americana',
+}
+{
+    id: 1,
+    title: 'Sanduiche Frango',
+    categoria: 'Sanduiche',
+    preco: 11.23,
+    imagem: '../assets/image/sanduiche/sanduiche-frango.jpg',
+    descricao: 'Iscas de peito de frango, alface, tomate, queijo e maionese americana',
+}
+{
+    id: 1,
+    title: 'Sanduiche Frango',
+    categoria: 'Sanduiche',
+    preco: 11.23,
+    imagem: '../assets/image/sanduiche/sanduiche-frango.jpg',
+    descricao: 'Iscas de peito de frango, alface, tomate, queijo e maionese americana',
+}
+{
+    id: 1,
+    title: 'Sanduiche Frango',
+    categoria: 'Sanduiche',
+    preco: 11.23,
+    imagem: '../assets/image/sanduiche/sanduiche-frango.jpg',
+    descricao: 'Iscas de peito de frango, alface, tomate, queijo e maionese americana',
+}
+]
